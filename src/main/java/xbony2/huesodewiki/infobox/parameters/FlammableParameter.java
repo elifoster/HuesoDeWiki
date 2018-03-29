18496630552990e340dbe8f3c45c09806a1aa8df
elifoster <elifosterwy@gmail.com>
2018-03-28 17:47:20 -0700

    Fix FlammableParameter

--- src/main/java/xbony2/huesodewiki/infobox/parameters/FlammableParameter.java
+++ src/main/java/xbony2/huesodewiki/infobox/parameters/FlammableParameter.java
@@ -2,6 +2,7 @@ package xbony2.huesodewiki.infobox.parameters;
 
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.util.ForgeDirection;
 import xbony2.huesodewiki.api.infobox.IInfoboxParameter;
 
 public class FlammableParameter implements IInfoboxParameter {
@@ -9,7 +10,7 @@ public class FlammableParameter implements IInfoboxParameter {
 	@Override
 	public boolean canAdd(ItemStack itemstack){
 		try {
-			return itemstack.getItem() instanceof ItemBlock && ((ItemBlock)itemstack.getItem()).getBlock().isFlammable(null, null, null);
+			return itemstack.getItem() instanceof ItemBlock && ((ItemBlock)itemstack.getItem()).field_150939_a.isFlammable(null, 0, 0, 0, ForgeDirection.UNKNOWN);
 		}catch(Exception e){
 			return false;
 		}
